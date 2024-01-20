4a3c59fc311d29d9ab7a75504bfe323e009ef033
Shrish-M <mahesh49@purdue.edu>
2024-01-20 17:07:50 -0500

    Saving changes

--- scripts/background.js
+++ scripts/background.js
@@ -4,12 +4,17 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'scrollEvent') {
       storedScrollData = message.data; // Store scroll count
-
+      console.log("Scroll Data "+ storedScrollData)
       // Check if popup is open and forward data
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.some((tab) => tab.url === chrome.runtime.getURL('popup.html'))) {
-          chrome.tabs.sendMessage(tabs[0].id, { type: 'scrollData', data: storedScrollData });
-          storedScrollData = []; // Clear stored data
+        const popupTab = tabs.find((tab) => tab.url === chrome.runtime.getURL('popup.html'));
+      
+        if (popupTab) {
+          chrome.tabs.sendMessage(popupTab.id, { type: 'scrollData', data: storedScrollData });
+        } else {
+          // Handle the case where the popup is not open
+          console.log("Popup is not currently open.");
+          // You might consider storing the data temporarily or taking other actions
         }
       });
     }
